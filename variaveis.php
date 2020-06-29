de1608ce73fc40f4a624065be01ebf8f70f24891
Brena Sarzi <brenasarzi32@gmail.com>
2020-06-29 12:54:27 -0300

    inicio do repositorio

---
+++ variaveis.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        // PHP precisa de ponto e virgula no final ";"
+
+        // Criando uma variavel e defindo o valor dela
+        $string = '<p>Isso é uma <strong>string</strong></p>';
+        // Imprimir variavel
+        echo $string;
+        // Imprimir uma tag html
+        echo '<br/>';
+        // mostra o tipo a quantidade e o conteúdo da variavel
+        var_dump($string);
+    ?> 
+
+</body>
+</html>
