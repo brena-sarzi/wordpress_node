ccea6716599903ce4c5325be4e103216b9f47aa4
gnew <brenasarzi32@gmail.com>
2020-06-29 16:43:02 -0300

    funcoes

---
+++ funcoes.php
@@ -0,0 +1,25 @@
+<?php
+
+function lista_padrao_site( $array_itens ){
+    $output = '';
+    if(! empty())
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções</title>
+</head>
+<body>
+
+    <?php 
+
+
+
+    ?>
+
+</body>
+</html>
